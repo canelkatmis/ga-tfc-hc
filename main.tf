18983bc6880828f75005ac08389745afe4d5f592
Can Elkatmış <can.elkatmis@gantek.com>
2024-03-01 12:53:47 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,10 @@ resource "huaweicloud_vpc" "myvpc" {
   name = "terraform_vpc"
   cidr = "192.168.0.0/16"
 }
-
+resource "huaweicloud_vpc" "myvpct" {
+  name = "terraform_vpc"
+  cidr = "192.169.0.0/16"
+}
 # An example resource that does nothing.
 resource "null_resource" "example" {
     triggers = {
