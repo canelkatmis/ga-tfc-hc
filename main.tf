55af357a31da1f3ab3bad1b191eadf634221b05d
Can Elkatmış <can.elkatmis@gantek.com>
2024-03-01 12:59:15 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,16 +17,14 @@ provider "huaweicloud" {
   region     = "ap-southeast-3"
 }
 
-resource "huaweicloud_vpc" "myvpc" {
-  name = "terraform_vpc"
-  cidr = "192.168.0.0/16"
-}
-resource "huaweicloud_vpc" "myvpct" {
-  name = "terraform_vpc"
-  cidr = "192.169.0.0/16"
-}
 # An example resource that does nothing.
 resource "null_resource" "example" {
+    triggers = {
+    value = "A example resource that does nothing!"
+    }
+}
+
+resource "null_resource" "exampletwo" {
     triggers = {
     value = "A example resource that does nothing!"
     }
